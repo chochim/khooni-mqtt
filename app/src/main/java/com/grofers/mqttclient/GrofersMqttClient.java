3409acb977eca26f858cd4bb1548cebaf6b59c97
Rohit Prakash <rohit.prakash@grofers.com>
2016-01-10 19:32:02 +0530

    initial commit

---
+++ app/src/main/java/com/grofers/mqttclient/GrofersMqttClient.java
@@ -0,0 +1,25 @@
+package com.grofers.mqttclient;
+
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClientPersistence;
+import org.eclipse.paho.client.mqttv3.MqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.internal.wire.MqttPingReq;
+
+/**
+ * Created by rohit on 10/01/16.
+ */
+public class GrofersMqttClient extends MqttClient {
+
+    public GrofersMqttClient(String serverUrl, String clientId, MqttClientPersistence persistence)
+            throws MqttException {
+        super(serverUrl, clientId, persistence);
+    }
+
+    public void ping() throws MqttException {
+        MqttDeliveryToken token = new MqttDeliveryToken(getClientId());
+        MqttPingReq pingMsg = new MqttPingReq();
+        aClient.comms.sendNoWait(pingMsg, token);
+    }
+}
